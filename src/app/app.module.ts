7e97d04b2b58beba34c196af9d1c960a416e7fad
Stonemannen <lucas.drufva@live.se>
2018-06-26 10:42:22 +0200

    Ändrar igen!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BLE } from '@ionic-native/ble';
 
 @NgModule({
   declarations: [
