f233e2875bffe98ab36d1be6472d304baee833ce
JonasTheCuriousDev <gamerage15@gmail.com>
2018-06-28 11:01:46 +0200

    micro:bit styrs nu av telefon via bluetooth. Knapparna på appen är justerade för att enkelt kunna användas. En uppdatering som filtrerar bort andra enheter utöver micro:biten är även implementerad. "on-hold" funktion för knappar är under konstruktion.

--- src/pages/selected/selected.ts
+++ src/pages/selected/selected.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { LoadingController } from 'ionic-angular';
-import { BLE } from '@ionic-native/ble';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { LoadingController } from "ionic-angular";
+import { BLE } from "@ionic-native/ble";
 
 @IonicPage()
 @Component({
-  selector: 'page-selected',
-  templateUrl: 'selected.html'
+  selector: "page-selected",
+  templateUrl: "selected.html"
 })
 export class SelectedPage {
   setStatus: any;
@@ -14,14 +14,21 @@ export class SelectedPage {
   connecteddd: boolean = false;
   uartService: any;
   uartRXCharacteristic: any;
+
+  arrowLeftActive: boolean;
+  aroowForwardActive: boolean;
+  aroowRightActive: boolean;
+  arrowBackwardActive: boolean;
+
+  textEncoder = new TextEncoder();
   constructor(
     public navCtrl: NavController,
     private ble: BLE,
     public loading: LoadingController,
     public navParams: NavParams
   ) {
-    let device = navParams.get('device');
-    this.setStatus = 'Shaking hand with ' + device.name + ' ID: ' + device.id;
+    let device = navParams.get("device");
+    this.setStatus = "Shaking hand with " + device.name + " ID: " + device.id;
     // UUID ex. FF11 = 1 on && 0 off
 
     this.ble
@@ -34,7 +41,7 @@ export class SelectedPage {
   // Loader
   presentloading() {
     const loader = this.loading.create({
-      content: 'Ansluter till apparat',
+      content: "Ansluter till apparat",
       duration: 500
     });
     loader.present();
@@ -45,10 +52,10 @@ export class SelectedPage {
     //alert("Handshake complete");
     this.peripheral = peripheral; // Peripheral is the slave && central master
     this.uartService = peripheral.services.find(element => {
-      return element.includes('b5a');
+      return element.includes("b5a");
     });
 
-    console.log('uartservice:' + JSON.stringify(this.uartService));
+    console.log("uartservice:" + JSON.stringify(this.uartService));
 
     // var found = array1.find(function (element) {
     //   return element > 10;
@@ -59,15 +66,15 @@ export class SelectedPage {
     for (let i = 0; i < peripheral.characteristics.length; i++) {
       let currentCrtscs = peripheral.characteristics[i];
       if (currentCrtscs.service == this.uartService) {
-        if (currentCrtscs.characteristic.includes('0003')) {
-          console.log('Setting RX chracteristic!');
+        if (currentCrtscs.characteristic.includes("0003")) {
+          console.log("Setting RX chracteristic!");
           this.uartRXCharacteristic = currentCrtscs.characteristic;
         }
         console.log(JSON.stringify(currentCrtscs));
       }
     }
     console.log(
-      'RX characteristic: ' + JSON.stringify(this.uartRXCharacteristic)
+      "RX characteristic: " + JSON.stringify(this.uartRXCharacteristic)
     );
     // this.uartRXCharacteristic =
     this.connecteddd = true;
@@ -76,7 +83,7 @@ export class SelectedPage {
 
   onDeviceDisconnected(peripheral) {
     this.peripheral = undefined;
-    alert('Handshake stopped');
+    alert("Handshake stopped");
     this.connecteddd = false;
   }
 
@@ -90,21 +97,44 @@ export class SelectedPage {
   }
 
   ionViewDidLoad() {
+    setInterval(() => { 
+      ///Let's here check if we should send drive stuff to the robot
+      if()
+     }, 100);
     this.presentloading();
-    console.log('ionViewDidLoad SelectedPage');
+    console.log("ionViewDidLoad SelectedPage");
   }
 
-  sender() {
-    console.log('gonna send BLE stuuufffzzz!');
-    let msg: string = '1\n';
-    let textEncoder = new TextEncoder();
+  send(msg) {
+    console.log("gonna send BLE stuuufffzzz!");
     // let buffer = new Uint8Array([msg]).buffer;
-    let buffer = textEncoder.encode(msg).buffer;
+    let buffer = this.textEncoder.encode(msg).buffer;
     this.ble.write(
       this.peripheral.id,
       this.uartService,
       this.uartRXCharacteristic,
       buffer
     );
+  }
+
+  forward() {
+    var msg: string = "0\n";
+    this.send(msg);
+  }
+  backward() {
+    var msg: string = "1\n";
+    this.send(msg);
+  }
+  left() {
+    var msg: string = "3\n";
+    this.send(msg);
+  }
+  right() {
+    var msg: string = "2\n";
+    this.send(msg);
+  }
+  stop() {
+    var msg: string = "4\n";
+    this.send(msg);
   }
 }
