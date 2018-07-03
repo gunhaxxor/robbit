83d24c2d9d226502c79e7bd3f782704e9abf9563
HACKING-STATION\gunhaxxor <gunnar.oledal@gmail.com>
2018-07-03 16:36:05 +0200

    getUserMedia Works!!!! Now let's consider if we'll go with phoneRTC or build it all ourselves :-P

--- src/pages/peer/peer.ts
+++ src/pages/peer/peer.ts
@@ -10,6 +10,9 @@ import { Camera, CameraOptions } from "@ionic-native/camera";
   templateUrl: "peer.html"
 })
 export class PeerPage {
+  public video: any;
+  private stream: any;
+
   constructor(
     platform: Platform,
     public navCtrl: NavController,
@@ -37,24 +40,34 @@ export class PeerPage {
   // });
 
   requestUserMedia() {
-    var constraints = { audio: true, video: true };
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(mediaStream => {
-        console.log("got mediadevice!");
-        var video = document.querySelector("video");
-        video.srcObject = mediaStream;
-        // video.onloadedmetadata = function(e) {
-        //   video.play();
-        // };
-      })
-      .catch(function(err) {
-        console.log("error vid mediadevice request");
-        console.log(err.name + ": " + err.message);
-      }); // always check for errors at the end.
+    this.video = document.querySelector("video");
+    const constraints = {
+      audio: false,
+      video: {
+        width: { min: 1024, ideal: 1280, max: 1920 },
+        height: { min: 776, ideal: 720, max: 1080 },
+        facingMode: "environment"
+      }
+    };
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      console.log("Found the getUserMedia API in the context");
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(stream => {
+          this.video.src = window.URL.createObjectURL(stream);
+          this.stream = stream;
+          this.video.play();
+        })
+        .catch(function(err) {
+          console.error(err.name + ": " + err.message);
+        });
+    } else {
+      console.error("Your browser does not support the mediaDevices API");
+    }
   }
 
-  cameraTriggered() {
+  requestCameraPermission() {
     // Prefer camera resolution nearest to 1280x720.
 
     // this.androidPermissions
@@ -67,49 +80,36 @@ export class PeerPage {
     //       )
     //   );
 
-    // this.androidPermissions
-    //   .requestPermission(this.androidPermissions.PERMISSION.CAMERA)
-    //   .then(
-    //     result => {
-    //       console.log("got it");
-    //       console.log(JSON.stringify(result));
-    //     },
-    //     err => {
-    //       console.log("didn't get it!!!");
-    //       console.log(JSON.stringify(err));
-    //     }
-    //   );
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    };
+    this.androidPermissions
+      .requestPermission(this.androidPermissions.PERMISSION.CAMERA)
+      .then(
+        result => {
+          console.log("got it");
+          console.log(JSON.stringify(result));
+        },
+        err => {
+          console.log("didn't get it!!!");
+          console.log(JSON.stringify(err));
+        }
+      );
 
-    this.camera.getPicture(options).then(
-      imageData => {
-        // imageData is either a base64 encoded string or a file URI
-        // If it's base64 (DATA_URL):
-        let base64Image = "data:image/jpeg;base64," + imageData;
-      },
-      err => {
-        // Handle error
-      }
-    );
     // const options: CameraOptions = {
     //   quality: 100,
     //   destinationType: this.camera.DestinationType.FILE_URI,
     //   encodingType: this.camera.EncodingType.JPEG,
     //   mediaType: this.camera.MediaType.PICTURE
-    // }
+    // };
 
-    // this.camera.getPicture(options).then((imageData) => {
-    //  // imageData is either a base64 encoded string or a file URI
-    //  // If it's base64 (DATA_URL):
-    //  let base64Image = 'data:image/jpeg;base64,' + imageData;
-    // }, (err) => {
-    //  // Handle error
-    // });
+    // this.camera.getPicture(options).then(
+    //   imageData => {
+    //     // imageData is either a base64 encoded string or a file URI
+    //     // If it's base64 (DATA_URL):
+    //     let base64Image = "data:image/jpeg;base64," + imageData;
+    //   },
+    //   err => {
+    //     // Handle error
+    //   }
+    // );
   }
 
   ionViewDidLoad() {
