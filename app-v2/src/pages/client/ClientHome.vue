9e143a7412e7e35be4c4a9374c0aed075283efd7
Gunnar Oledal <gunnar.oledal@gmail.com>
2021-04-12 18:52:04 +0200

    first commit

---
+++ app-v2/src/pages/client/ClientHome.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page padding>
+    <h3>
+      Hello {{ person }}!
+    </h3>
+    <q-input v-model="person" />
+  </q-page>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref
+} from 'vue'
+
+export default defineComponent({
+  name: 'ClientHome',
+  components: {},
+  setup () {
+    const person = ref<string>('stranger')
+
+    return {
+      person
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+</style>
