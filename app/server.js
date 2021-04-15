8eb1c9754b7e11fc8abf75c90ecca446a5eeeefc
gunhaxxor on server <gunnar.oledal@gmail.com>
2021-04-15 09:16:51 +0000

    Longer timeout for failing peer init. Printing env in signaling server. A lot of settings for coturn in compose.

--- app/server.js
+++ app/server.js
@@ -8,5 +8,5 @@ server.use(express.static(__dirname + '/www'));
 server.listen(8080);
 
 console.log('YOOOOOO! Started web server for static files');
-// console.log('This is the environment:');
-// console.log(process.env);
+console.log('This is the environment:');
+console.log(process.env);
