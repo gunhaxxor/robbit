5e70fc89f8cf932d7ad6ea1bbde6c93783df9b2f
Gunnar Oledal <gunnar.oledal@gmail.com>
2021-11-03 10:30:53 +0100

    working with project structure for dev vs prod

---
+++ backend-v2/auth/entrypoint.sh
@@ -0,0 +1,14 @@
+# #!/bin/bash
+# if [ -z ${DEV+x}]
+# then
+#   echo 'FUCK Yeah! We are liiiive!'
+#   npm start
+# else
+#   echo 'This is development. No need to worry!!'
+#   npm run dev
+# fi
+
+# # echo "Dev is: ${DEV}"
+# # printenv DEV
+
+npm start
